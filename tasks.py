126ac8ac3a478e4d6657f758eaff2b23a7bfad18
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-09-11 14:11:31 -0700

    iterating

--- tasks.py
+++ tasks.py
@@ -7,4 +7,12 @@ def buildlocal(ctx):
 # export BUILDKITE_AGENT_TOKEN= value from Buildkite
 @task
 def runlocalagent(ctx):
-    ctx.run("docker run -v \"/var/run/docker.sock:/var/run/docker.sock\" -e NITE -d -t local/buildkite-infra:latest start --token $BUILDKITE_AGENT_TOKEN  --tags \"queue=bootstrap\"")
+    LAUNCH = "docker run -v \"/var/run/docker.sock:/var/run/docker.sock\" " \
+             "-e NITE " \
+             "-e NITETIME " \
+             "-d -t local/buildkite-infra:latest start " \
+             "--token $BUILDKITE_AGENT_TOKEN " \
+             "--tags \"queue=bootstrap\" "
+
+    ctx.run('source local.env')
+    ctx.run(LAUNCH)
